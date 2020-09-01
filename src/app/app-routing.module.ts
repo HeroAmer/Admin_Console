72ddb5200367e08b45e6ed8a8d56f4e0f5dc05f1
Elvir9 <ehalilovicc@gmail.com>
2020-09-01 17:43:23 +0200

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomePageComponent
+  },
+  {
+    path: 'create', component: CreatePostComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+export const routingComponents = [CreatePostComponent]
