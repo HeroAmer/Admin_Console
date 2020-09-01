a9e13738e1a57be78343191bc6175e236fb92669
Amer Hero <amherhero@gmail.com>
2020-09-01 15:33:21 +0200

    create post component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './create-post/create-post.component';
@@ -22,7 +23,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
+  form:FormGroup;
+  imagePreview:string;
+  posts=[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, {validators:[Validators.required]}),
+      content : new FormControl(null, {validators:[Validators.required]}),
+      image : new FormControl(null, {validators:[Validators.required]})
+    });
+
   }
+  onImagePicked(event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image:file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSavePost(){
+    console.log("Submit was clicked");
+    if(this.form.invalid){
+      return;
+    }else{
+      const post = {
+        image:'../assets/noPhoto.jpg',
+        title: this.form.value.title,
+        content: this.form.value.content
+      }
+      this.posts.push(post);
+      console.log(this.posts);
+    }
+
+    this.form.reset();
+  }
+
+
 
 }
