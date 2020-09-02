731b367aa5ff27f519dc13284663e66db28ffba6
Elvir9 <ehalilovicc@gmail.com>
2020-09-03 00:55:53 +0200

    transform id

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
@@ -12,11 +13,21 @@ export class PostService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              title: post.title,
+              image: post.image,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
       )
-      .subscribe((newData) => {
-        this.posts = newData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
