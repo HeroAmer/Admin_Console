7a779b54380a0318a4f251f12fbdd46a83b5b284
Elvir9 <ehalilovicc@gmail.com>
2020-09-07 14:23:19 +0200

    admin panel edit

--- src/app/admin-panel/preview-users/preview-users.component.ts
+++ src/app/admin-panel/preview-users/preview-users.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../../post.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-preview-users',
   templateUrl: './preview-users.component.html',
-  styleUrls: ['./preview-users.component.css']
+  styleUrls: ['./preview-users.component.css'],
 })
 export class PreviewUsersComponent implements OnInit {
+  posts = [];
+  private postSubscription: Subscription;
 
-  constructor() { }
+  constructor(public postService: PostService) {}
 
   ngOnInit(): void {
+    console.log('nestoo');
+    this.postService.getPosts();
+    console.log(this.posts);
+    this.postSubscription = this.postService
+      .getPostsUpdatedListener()
+      .subscribe((posts) => {
+        this.posts = posts;
+      });
   }
 
+  onEditPost() {}
+
+  ngOnDestroy() {
+    this.postSubscription.unsubscribe();
+  }
+  showPosts() {
+    console.log(this.posts);
+  }
+  onDelete(id: string) {
+    this.postService.deletePost(id);
+    alert('Post Deleted!');
+  }
+  refreshPage() {
+    window.location.reload();
+  }
 }
